da354dcfb1c9f9046ba9aa6b6d0057e621903538
Dluhj <obinnaedmund39@gmail.com>
2024-05-09 06:55:54 -0700

    added content to the contact page and made some minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,18 +51,11 @@
     onscroll(document, toggleBacktotop);
   }
 
-  /**
-   * Mobile nav toggle
-   */
   on("click", ".mobile-nav-toggle", function (e) {
     select("#navbar").classList.toggle("navbar-mobile");
     this.classList.toggle("bi-list");
     this.classList.toggle("bi-x");
   });
-
-  /**
-   * Mobile nav dropdowns activate
-   */
   on(
     "click",
     ".navbar .dropdown > a",
@@ -75,57 +68,7 @@
     true
   );
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on(
-    "click",
-    ".scrollto",
-    function (e) {
-      if (select(this.hash)) {
-        e.preventDefault();
-
-        let navbar = select("#navbar");
-        if (navbar.classList.contains("navbar-mobile")) {
-          navbar.classList.remove("navbar-mobile");
-          let navbarToggle = select(".mobile-nav-toggle");
-          navbarToggle.classList.toggle("bi-list");
-          navbarToggle.classList.toggle("bi-x");
-        }
-        scrollto(this.hash);
-      }
-    },
-    true
-  );
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener("load", () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash);
-      }
-    }
-  });
-
-  // HERO SWIPER
-
-  var imageSliderOne = new Swiper(".image-slider-1", {
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    loop: true,
-    slidesPerView: "1",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  /**
-   * Testimonials slider
-   */
+  // TESTIMONIAL SLIDER
   new Swiper(".testimonials-slider", {
     speed: 600,
     loop: true,
@@ -152,68 +95,6 @@
     },
   });
 
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: ".portfolio-item",
-        layoutMode: "fitRows",
-      });
-
-      let portfolioFilters = select("#portfolio-flters li", true);
-
-      on(
-        "click",
-        "#portfolio-flters li",
-        function (e) {
-          e.preventDefault();
-          portfolioFilters.forEach(function (el) {
-            el.classList.remove("filter-active");
-          });
-          this.classList.add("filter-active");
-
-          portfolioIsotope.arrange({
-            filter: this.getAttribute("data-filter"),
-          });
-          portfolioIsotope.on("arrangeComplete", function () {
-            AOS.refresh();
-          });
-        },
-        true
-      );
-    }
-  });
-
-  /**
-   * Initiate portfolio lightbox
-   */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
-  /**
-   * Animation on scroll
-   */
   window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
@@ -222,23 +103,4 @@
       mirror: false,
     });
   });
-
-  /**
-   * Initiate Pure Counter
-   */
-  new PureCounter();
 })();
-
-// HERO PLAY BUTTON
-const heroVideo = document.getElementById("heroVideo");
-const HeroPlay = document.getElementById("HeroPlay");
-
-function play() {
-  if (heroVideo.paused) {
-    heroVideo.play();
-    HeroPlay.style.background = "url(/assets/img/svgPlay.png)";
-  } else {
-    heroVideo.pause();
-    HeroPlay.style.background = "url(/assets/img/svgPause.png)";
-  }
-}
